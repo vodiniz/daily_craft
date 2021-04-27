9df2513ba4d9d579b8352092c3e3cdfd7f5ee508
Vodiniz <vitor.oliveira.vod@gmail.com>
2021-04-27 09:22:00 -0300

    v1.3

--- efficiency_Craft.py
+++ efficiency_Craft.py
@@ -13,54 +13,40 @@ urls=[
     'https://wiki.guildwars2.com/wiki/Square_of_Vabbian_Silk'
 ]
 
-def create_itens(urls):
-    itens = []
+def create_items(urls):
+    items = []
 
     for url in urls:
-    	gw2_wiki = requests.get(url)
-   		soup_page = BeautifulSoup(gw2_wiki.content,'html.parser')
+        gw2_wiki = requests.get(url)
+        soup_page = BeautifulSoup(gw2_wiki.content,'html.parser')
 
-   		name = get_item_name(soup_page)
+        name = get_item_name(soup_page)
+        id = get_id(name,json_id_list)
+        sellprice, buyprice = get_api_price(item['id'])
+        recipe_url = get_item_recipe_url(soup)
 
-		item = {
+        item = {
             'name': name,
-            'id': None,
+            'id': id,
             'url': url,
-            'sellprice': None,
-            'buyprice': None, 
-            'recipe':None
+            'sellprice': sellprice,
+            'buyprice': buyprice, 
+            'recipe_url':recipe_url
         }
 
-        itens.append(item)
+        items.append(item)
         set_names.append(name)
 
-    return itens
-
-
-def get_item_details(itens):
-	for item in itens:
-
-	    gw2_wiki = requests.get(item['url'])
-	    soup_page = BeautifulSoup(gw2_wiki.content,'html.parser')
-
-	    sellprice, buyprice = get_api_price(item['id'])
-	    item['sellprice'] = sellprice
-	    item['buyprice'] = buyprice
-	    item['recipe'] = get_item_recipe(soup)
+    return items
 
 def get_item_name(soup):
-	name = soup_page\
+    name = soup_page\
         .find('h1', class_="firstHeading")\
         .get_text()
     return name
 
-def get_item_id(name):
-	for key in item_json_list:
-		if(name == key.get())
-			return key.get()
-
-def get_item_recipe(soup):
-	soup_page\
+def get_item_recipe_url(soup):
+    soup_page\
     .find('span',{'class':'plainlinks'})\
     .find('a', recursive = False)\
     .get('href')
@@ -68,7 +54,7 @@ def get_item_recipe(soup):
     return recipe
 
 def get_api_price(id):
-	api_price_url = 'https://api.guildwars2.com/v2/commerce/prices?ids={}&wiki=1&lang=en'.format(id)
+    api_price_url = 'https://api.guildwars2.com/v2/commerce/prices?ids={}&wiki=1&lang=en'.format(id)
     response = requests.get(api_price_url)
     json = response.json()
     result = json[0]['sells']['unit_price']
@@ -76,49 +62,90 @@ def get_api_price(id):
 
     return result, result2
 
-def get_all_ids(set):
-	url_item_id = 'http://api.gw2tp.com/1/bulk/items-names.json'
-	url_item_id_response = requests.get(url_item_id)
-	json = requests.json()
+def get_json_id_list():
+    url_item_id = 'http://api.gw2tp.com/1/bulk/items-names.json'
+    url_item_id_response = requests.get(url_item_id)
+    json_id_list = url_item_id_response.json()
 
-	for item in ['items']
-		if ''
-
-
-	return
+    return json_id_list
 
 
-def item_id_list():
+def get_id(name,dic_list):
 
-
-	return json
-
+    for key, dkey in dic_list.items():
+        for item in dkey:
+            if (item[1] == name):
+                return item[0]
 
 def get_recipe_list(item):
-	recipe_list = []
-	recipe_ingredients = {}
-	url_recipe = item['recipe']
+    recipe_list = []
+    recipe_ingredients = {}
+    url_recipe = item['recipe_url']
 
-	recipe_request = url_recipe.requests
-	recipe_soup = BeautifulSoup(recipe_request.content,'html.parser')
-	soup_recipe_list = recipe.soup_page.\
-	find('span',{'class':'small item-icon thumb-icon'})\
-	.find('a', recursive = False)\
-    .get('href')
+    recipe_request = url_recipe.requests.get()
+    recipe_soup = BeautifulSoup(recipe_request.content,'html.parser')
+    soup_recipe_list = recipe_soup\
+    .find('tbody')
 
-def print_list_dict(itens):
-    for item in itens:
+    soup_recipe_name = soup_recipe_list.find_all('span',{'class':'small item-icon thumb-icon'})
+    soup_recipe_quantity = soup_recipe_list.find_all('tr',{'class':'tptotal'})
+    zip_recipe = zip(soup_recipe_name,soup_recipe_quantity)
+    index = 0
+
+    for element1, element2 in zip_recipe:
+        soup_recipe_name[index] = element1.find('a').get('title')
+        soup_recipe_quantity[index] = element2.get('data-qty') 
+        recipe_ingredients[get(id)] = [soup_recipe_name[index],soup_recipe_quantity[index]]
+        index += 1
+
+    print_list_dict(recipe_ingredients)
+
+def recipe_list_all(items):
+    recipe_list = []
+    recipe_ingredients
+    #for item in items:
+
+
+        index += 1
+
+def get_specific_recipe():
+    url_recipe = 'https://wiki.guildwars2.com/index.php?title=Special:RunQuery/Base_ingredients_query&Base_ingredients%5Bitem%5D=Deldrimor%20Steel%20Ingot&Base_ingredients%5Bid%5D=7313&Base_ingredients%5Bquantity%5D=1&_run'
+
+    recipe_request = requests.get(url_recipe)
+    recipe_soup = BeautifulSoup(recipe_request.content,'html.parser')
+    soup_recipe_list = recipe_soup\
+    .find('tbody')
+
+    soup_recipe_name = soup_recipe_list.find_all('span',{'class':'small item-icon thumb-icon'})
+    soup_recipe_quantity = soup_recipe_list.find_all('tr',{'class':'tptotal'})
+
+
+    zip_object = zip(soup_recipe_name,soup_recipe_quantity)
+
+    index = 0
+    for element1, element2 in zip_object:
+        soup_recipe_name[index] = element1.find('a').get('title')
+        soup_recipe_quantity[index] = element2.get('data-qty') 
+
+        index += 1
+
+
+
+def print_list_dict(items):
+    for item in items:
         for key, value in item.items():
             print(key, ' : ', value)
         print('--------')
 
 
 def main():
-	set_names = set()
-    itens = create_itens(urls)
-    get_all_ids(set_names)
+    #set_names = set()
+    #json_id_list = get_json_id_list()
+    #items = create_items(urls)
 
-    print_list_dict(itens)
+    #get_all_ids(set_names)
+    get_specific_recipe2()
+    #print_list_dict(items)
 
 
 
